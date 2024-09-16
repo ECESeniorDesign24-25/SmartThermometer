30eb76d9e0fd38d36a0ef7fcde5be4a16742296a
jkrue242 <josephkrueger242@gmail.com>
2024-09-16 15:13:11 -0500

    iniital web server (wip)

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,13 +1,30 @@
 #include <WiFi.h>
-#include <HTTPClient.h>
+#include <ESPAsyncWebServer.h> 
+#include "WiFiCredentials.h" // You need to update this file locally
+
+AsyncWebServer server(80); // Port 80
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  if (WIFI_USERNAME == "your_USERNAME" || WIFI_PASSWORD == "your_PASSWORD") {
+    Serial.println("Invalid username or password for wifi connection.");
+  }
 
+  WiFi.begin(WIFI_USERNAME, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.printf("Connected to WiFi with IP Address: %s\n", WiFi.localIP().toString().c_str());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "Hello from ESP32");
+  });
+
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Nothing here
 }
-

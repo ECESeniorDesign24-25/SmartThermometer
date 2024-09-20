2e1fdedaff658c99c0348c9a4499b34cdaa0f2b6
jkrue242 <josephkrueger242@gmail.com>
2024-09-19 20:47:04 -0500

    add OneWireMock

--- ESP32/SensorUtils.h
+++ ESP32/SensorUtils.h
@@ -1,15 +1,15 @@
 #ifndef SENSOR_UTILS_H
 #define SENSOR_UTILS_H
-
-#include <DallasTemperature.h>
-#include <OneWire.h>
 #include <string>
 
 #ifdef UNIT_TEST
   #include "../lib/ArduinoMock.h" 
   #include "../lib/DallasTemperatureMock.h" 
+  #include "../lib/OneWireMock.h"
 #else
   #include <Arduino.h>
+  #include <DallasTemperature.h>
+  #include <OneWire.h>
 #endif
 
 void checkButtonStatus(int buttonRead, bool& buttonOn, int& buttonState, int& lastButtonState, int debounceDelay, unsigned long& lastDebounceTime);

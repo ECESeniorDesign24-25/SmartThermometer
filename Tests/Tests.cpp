2b9652ec28ba8cf77d8532b66735d6764270c39e
jkrue242 <josephkrueger242@gmail.com>
2024-09-20 13:37:36 -0500

    add more tests

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -26,6 +26,15 @@ void testCalculateTemperature() {
 
     // check C -> C
     TEST_ASSERT_EQUAL_FLOAT(100.0, convertTemperature(100.0, "c", "c")); 
+
+    // check F -> F
+    TEST_ASSERT_EQUAL_FLOAT(100.0, convertTemperature(100.0, "f", "f"));
+
+    // check C -> F
+    TEST_ASSERT_EQUAL_FLOAT(212.0, convertTemperature(100.0, "c", "f"));
+
+    // check F -> C
+    TEST_ASSERT_EQUAL_FLOAT(37.777779, convertTemperature(100.0, "f", "c"));
 }
 
 
